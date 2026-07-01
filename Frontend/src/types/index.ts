790fad9ab38c6aba4e7c13961bc1c1d9555deb60
Cnnb01 <nyamambanyaguch@gmail.com>
2026-07-01 21:26:41 +0300

    refactor: adding helper code

---
+++ Frontend/src/types/index.ts
@@ -0,0 +1,19 @@
+export type Status = 'todo' | 'in_progress' | 'done'
+export type Priority = 'low' | 'medium' | 'high'
+export interface Project {
+    id: number,
+    name: string,
+    description: string,
+    created_at: string
+}
+ 
+export interface Task {
+    id: number,
+    title: string,
+    description: string,
+    due_date: string,
+    created_at: string,
+    status: Status,
+    priority:Priority, 
+    project_id: number
+}
