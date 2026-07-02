5c89f936f97193d675f3bbe6bf5c05d738a47b34
Cnnb01 <nyamambanyaguch@gmail.com>
2026-07-02 12:57:00 +0300

    feat: adding new pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
 import ProjectTasks from './pages/projectTasks'
+import CreateProject from './pages/createProject'
+import CreateTask from './pages/createTask'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard/>}/>
           <Route path="/projects/:id/tasks" element={<ProjectTasks/>}/>
+          <Route path="/create-project" element={<CreateProject/>}/>
+          <Route path="/projects/:id/create-task" element={<CreateTask/>}/>
         </Routes>
         </BrowserRouter>
       </section>
