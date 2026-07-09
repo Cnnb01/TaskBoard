5faf48073399279feac456342f5bc45e0bd30b30
Cnnb01 <nyamambanyaguch@gmail.com>
2026-07-09 13:13:26 +0300

    fix: adding new routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,26 +2,23 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
 import ProjectTasks from './pages/projectTasks'
-import CreateProject from './pages/createProject'
-import CreateTask from './pages/createTask'
-import EditProject from './pages/editProject'
-import EditTask from './pages/editTask'
+import ProjectForm from './components/project_form'
+import TaskForm from './components/task_form'
 
 function App() {
   return (
     <>
       <section id="center">
-        HELLLO
         <h1>WELCOME TO TASKBOARD</h1>
         <h2>TRACK YOUR TASKS</h2>
         <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard/>}/>
           <Route path="/projects/:id/tasks" element={<ProjectTasks/>}/>
-          <Route path="/create-project" element={<CreateProject/>}/>
-          <Route path="/projects/:id/create-task" element={<CreateTask/>}/>
-          <Route path="/projects/:id/edit-project" element={<EditProject/>}/>
-          <Route path="/projects/:id/tasks/:taskId/edit-task" element={<EditTask/>}/>
+          <Route path="/create-project" element={<ProjectForm mode="create"/>}/>
+          <Route path="/projects/:id/create-task" element={<TaskForm mode='create'/>}/>
+          <Route path="/projects/:id/edit-project" element={<ProjectForm mode="edit"/>}/>
+          <Route path="/projects/:id/tasks/:taskId/edit-task" element={<TaskForm mode="edit"/>}/>
         </Routes>
         </BrowserRouter>
       </section>
