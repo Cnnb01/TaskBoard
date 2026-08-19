cba1bf618d1dfa95f647b23d85269b15b84a8322
Cnnb01 <nyamambanyaguch@gmail.com>
2026-08-19 15:12:43 +0300

    refactor: remove unused type import from summary component

--- Frontend/src/pages/summary.tsx
+++ Frontend/src/pages/summary.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import api from "../api/client";
 import { useParams } from "react-router-dom";
-import type { Project } from "../types";
 import { AxiosError } from "axios";
 
 const Summary = () => {
