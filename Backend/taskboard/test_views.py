d8064165ff006d7908727efdad3e904fcf3930f5
Cnnb01 <nyamambanyaguch@gmail.com>
2026-08-22 17:25:04 +0300

    feat: add tests for Project view to verify response status and data structure

--- Backend/taskboard/test_views.py
+++ Backend/taskboard/test_views.py
@@ -8,3 +8,10 @@ from taskboard.models import Project, Task
 class ProjectViewTest(APITestCase):
     def setUp(self):
         self.project = Project.objects.create(name="Test Project")
+    def test_get_projects_returns_200(self):
+        res = self.client.get('/api/v1/projects/')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_projects_returns_list(self):
+        res = self.client.get('/api/v1/projects/')
+        self.assertEqual(len(res.data['results']), 1)
